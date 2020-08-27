c99c0dd009c980e8c952c7f27b216ed7cd041392
Heath Blandford <heath.blandford@gmail.com>
2020-08-26 22:44:04 -0400

    trim header files

--- main/app_main.c
+++ main/app_main.c
@@ -1,29 +1,24 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stddef.h>
 #include <string.h>
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-#include "protocol_examples_common.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/queue.h"
-
-#include "lwip/sockets.h"
-#include "lwip/dns.h"
-#include "lwip/netdb.h"
-
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "esp_event.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
 #include "mqtt_client.h"
+#include "esp_http_client.h"
+#include "esp_tls.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 // Losant Credentials
-#define device_id     "<device_id>"
-#define access_key    "<access_key>"
+#define device_id "<device_id>"
+#define access_key "<access_key>"
 #define access_secret "<access_secret>"
 
 static const char *TAG = "ESP32_GETTING_STARTED";
